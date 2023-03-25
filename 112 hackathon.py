53719ad3c6fc06145b52f5f285b6258e1bfed6f5
noodlewoo <128935634+noodlewoo@users.noreply.github.com>
2023-03-25 17:55:42 -0400

    Update 112 hackathon.py

--- 112 hackathon.py
+++ 112 hackathon.py
@@ -6,6 +6,10 @@ def onAppStart(app):
 def reset(app):
     app.health = 57
     app.hygiene = 43
+    app.time = 8
+    app.timePeriod = 'am'
+    app.date = 'Mon'
+    app.text = 'Insert text here...'
 
 def onStep(app):
     pass
@@ -19,12 +23,16 @@ def redrawAll(app):
     drawRect(230, 10, 160, 40, fill='black')
     drawRect(230, 10, app.hygiene*1.6, 40, fill='green')
     
-    drawRect(10, 350, 40, 40, fill='yellow')
-    drawRect(60, 300, 330, 90, fill='cyan')
-    drawRect(10, 300, 40, 40, fill='lightgreen')
-    
-    drawLabel('8 am', 30, 320, size=12)
-    drawLabel('Tues', 30, 370, size=12)
+    #time
+    drawRect(10, 300, 40, 40, fill='grey')
+    drawLabel(f'{app.time} {app.timePeriod}', 30, 320, size=12)
+
+    #date
+    drawRect(10, 350, 40, 40, fill='grey')
+    drawLabel(app.date, 30, 370, size=12)
+
+    #time and date
+    drawRect(60, 300, 330, 90, fill='grey')
     drawLabel('Insert text here...', 225, 345, size=12)
 
 def onKeyPress(app, key):
